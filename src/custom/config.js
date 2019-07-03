7e942a414e3c3282dd6a0cbf13474dc14420ca60
Sebastian <sebastian.ovide@gmail.com>
2019-07-03 10:36:10 +0100

    sync photos

--- src/custom/config.js
+++ src/custom/config.js
@@ -124,7 +124,9 @@ const PAGES = {
   }
 };
 
-export default {
+const STATIC_CONFIG = require("./config.json");
+
+export default { ...STATIC_CONFIG,
   CUSTOM_STRING,
   MAX_IMAGE_SIZE: 2048,
   THEME: {
@@ -138,14 +140,6 @@ export default {
   // MAP_SOURCE: "https://s3-eu-west-1.amazonaws.com/tiles.os.uk/styles/open-zoomstack-outdoor/style.json",
   // MAP_ATTRIBUTION: "Contains OS data &copy; Crown copyright and database rights 2018",
   MAPBOX_TOKEN: "pk.eyJ1Ijoic2ViYXN0aWFub3ZpZGVnZW92YXRpb251ayIsImEiOiJjanBqZzRmNHgwNXljM2tydHlkM29id3FwIn0.-1V8Ue9P6eQr8FGghaTYiw",
-  FIREBASE: {
-    apiKey: "AIzaSyBbN8z-zSqChaQkTyOtIZZ3apq0qg59FzI",
-    authDomain: "plastic-patrol-fd3b3.firebaseapp.com",
-    databaseURL: "https://plastic-patrol-fd3b3.firebaseio.com",
-    projectId: "plastic-patrol-fd3b3",
-    storageBucket: "plastic-patrol-fd3b3.appspot.com",
-    messagingSenderId: "845679623528"
-  },
   GA_TRACKING_ID: "UA-126516084-1",
   PHOTO_ZOOMED_FIELDS: {
     "updated": s => new Date(s).toDateString(),
